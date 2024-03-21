102be945fad6987b2986da5c686888f4a58a2682
Deepak bhatt <157270970+deepak93o@users.noreply.github.com>
2024-03-21 08:24:14 +0530

    Add files via upload

---
+++ download.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Button click event to redirect to another page
+  document.getElementById("download").addEventListener("click", function() {
+      window.location.href = ".html";
+  });
+});
